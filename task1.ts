8576e8bbe22860b9dac4832e1f02c94f3297854d
ivserada <ivserada@gmail.com>
2026-09-22 21:56:20 +0300

    feat: lesson9 tasks

--- task1.ts
+++ task1.ts
@@ -1,6 +1,15 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Написать метод myReduce в прототипе массива, который будет работать аналогично reduce
 
 const array: number[] = [4, 2, 3, 1];
+Array.prototype.myReduce = function <T, R>(
+  func: (acc: R, value: T, index: number, array: T[]) => R,
+  initialValue?: R,
+): R {
+  let accumulator = initialValue ?? this[0];
+  for (let i = initialValue !== undefined ? 0 : 1; i < this.length; i++) {
+    accumulator = func(accumulator, this[i], i, this);
+  }
+  return accumulator;
+};
 
-//array.myReduce((acc, value) => acc + value); //10
+console.log(array.myReduce((acc, value) => acc + value)); //10
