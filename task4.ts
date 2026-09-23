7c41fb0ab6c4857fc9eddc815e0aa6b0e5738942
ivserada <ivserada@gmail.com>
2026-09-23 17:01:23 +0300

    feat: ;esson 9 hw

--- task4.ts
+++ task4.ts
@@ -1,5 +1,15 @@
 // Написать свой метод myFilter в прототипе массивов
-
 const arr = [1, 5, -4, 3, -2, 0];
+
+Array.prototype.myFilter = function <T>(func: (value: T, index: number, array: T[]) => boolean): T[] {
+  const result: T[] = [];
+  for (let index = 0; index < this.length; index++) {
+    const value = this[index];
+    if (func(value, index, this)) {
+      result.push(value);
+    }
+  }
+  return result;
+};
 
 console.log(arr.myFilter((value) => value > 0));
